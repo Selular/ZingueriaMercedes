71511330cc0d151141cfd7bb2b75aa0f5d93b9b5
Selular <selenapanessi@gmail.com>
2026-01-13 00:28:50 -0300

    Update products.ts

--- data/products.ts
+++ data/products.ts
@@ -2,6 +2,8 @@
 import { Product, Category, Brand } from '../types';
 
 export const products: Product[] = [
+ 
+  /**Lepen**/
   {
     id: 'l-01',
     name: 'Moquehue Vintage 9000 ', 
@@ -24,54 +26,385 @@ export const products: Product[] = [
   {
     id: 'l-03',
     name: 'Moquehue 20000 ',
-    description: 'Un clásico de la calefacción argentina, robusto y confiable.',
     price: 596.190,
     category: Category.CALEFACCION,
     brand: Brand.LEPEN,
-    imageUrl: './fotos/',
-    features: ['9000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
     isBestSeller: true
   },
-  {
-    id: 'z-01',
-    name: 'Canaleta Americana 25',
-    description: 'Canaleta de zinguería premium para techos de chapa o teja.',
-    price: 15000,
-    category: Category.ZINGUERIA,
-    brand: Brand.MERCEDES,
-    imageUrl: 'https://images.unsplash.com/photo-1635424710928-0544e8512eae?q=80&w=800&auto=format&fit=crop',
-    features: ['Chapa C25', 'Pintura Epoxi opcional', 'Varios colores']
-  },
-  {
-    id: 't-03',
-    name: 'Tromen Dupla',
-    description: 'Parrilla y horno a leña en una sola unidad compacta.',
-    price: 680000,
-    category: Category.COCINA,
-    brand: Brand.TROMEN,
-    imageUrl: 'https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=800&auto=format&fit=crop',
-    features: ['Acero inoxidable', 'Ladrillos refractarios', 'Termómetro'],
-    isBestSeller: true
-  },
-  {
-    id: 'z-02',
-    name: 'Kit Ventilación 6"',
-    description: 'Kit completo para salida de humos en estufas a leña.',
-    price: 45000,
-    category: Category.ZINGUERIA,
-    brand: Brand.MERCEDES,
-    imageUrl: 'https://images.unsplash.com/photo-1584622650111-993a426fbf0a?q=80&w=800&auto=format&fit=crop',
-    features: ['Enlozado', 'Sombrerete Venturi', 'Abrazaderas incluidas'],
-    isBestSeller: true
-  },
-  {
-    id: 'l-02',
-    name: 'Lepen Horno Tromen Adaptable',
-    description: 'Horno de embutir de alto rendimiento Lepen.',
-    price: 520000,
-    category: Category.COCINA,
+ {
+    id: 'l-04',
+    name: 'Rond 7000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
     brand: Brand.LEPEN,
-    imageUrl: 'https://images.unsplash.com/photo-1527334134460-f21a05ef52a3?q=80&w=800&auto=format&fit=crop',
-    features: ['Gran capacidad', 'Reloj de temperatura', 'Bandejas incluidas']
-  }
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+ 
+   {
+    id: 'l-05',
+    name: 'Rond 7000 Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-06',
+    name: '7000 Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-07',
+    name: 'Moquehue 8000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-08',
+    name: 'Moquehue 13500 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-09',
+    name: 'Moquehue 13500 Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-10',
+    name: '10000 Rinconero ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+
+  {
+    id: 'l-11',
+    name: '11000 Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-12',
+    name: '12000 Premium Con Horno',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-13',
+    name: '14000 Alta Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+
+  {
+    id: 'l-14',
+    name: '14000 Alta Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-15',
+    name: '14000 Alta Premium Con Base Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-16',
+    name: '15000 Rinconero ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-17',
+    name: '17000 Premium Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-18',
+    name: '19000 Rinconero Premium Con Horno',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-19',
+    name: '20000 Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-20',
+    name: '27000 Premium',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-21',
+    name: '27000 Rinconero Premium',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-22',
+    name: '27000 Rinconero Premium Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-23',
+    name: 'Campana 27000 Premium',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+
+  {
+    id: 'l-24',
+    name: 'Campana 27000 Doble Puerta Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-25',
+    name: '14000 Alta Premium ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+    {
+    id: 'l-26',
+    name: 'Nº 1 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-27',
+    name: 'Nº 2 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-28',
+    name: 'Nº 3 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-29',
+    name: 'Nº 4 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-30',
+    name: 'Nº 5 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-31',
+    name: 'Nº 6 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-32',
+    name: 'Nº 4 Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-33',
+    name: 'Nº 5 Con Horno',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-34',
+    name: 'Nº 5 Con Horno ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+   {
+    id: 'l-35',
+    name: 'Central 15000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-36',
+    name: 'Central 20000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-36',
+    name: 'Central 25000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-37',
+    name: 'Central 30000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-38',
+    name: 'Central 39000 ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
+  {
+    id: 'l-39',
+    name: 'Four Face ',
+    price: 596.190,
+    category: Category.CALEFACCION,
+    brand: Brand.LEPEN,
+    imageUrl: './fotos/3.png',
+    features: ['20000 kcal/h', 'Refractarios de alta calidad', 'Manija fría'],
+    isBestSeller: true
+  },
 ];
