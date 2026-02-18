21a9fb7570555dd554784a0057b53e224ced1c87
Selular <selenapanessi@gmail.com>
2026-02-18 13:54:21 -0300

    fix en types

--- types.ts
+++ types.ts
@@ -28,7 +28,7 @@ export interface Product {
   id: string;
   name: string;
   price: number;
-  installments?: number[]; // Cantidad de cuotas disponibles, ej: [3, 6]
+  installments?: number[]; // Cantidad de cuotas disponibles, ejemplito: [3, 6]
   category: Category;
   brand: Brand;
   imageUrl: string;
